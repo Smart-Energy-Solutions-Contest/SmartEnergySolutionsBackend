c5edd19e1b9746c1e4f601a0925a2cd882779b26
BalalauBogdan <bogdan262003@gmail.com>
2025-02-19 12:44:47 +0200

    Add .env file for security

--- src/main/java/org/sesolutions/smartenergysolutionsbackend/SmartEnergySolutionsBackendApplication.java
+++ src/main/java/org/sesolutions/smartenergysolutionsbackend/SmartEnergySolutionsBackendApplication.java
@@ -1,13 +1,19 @@
 package org.sesolutions.smartenergysolutionsbackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartEnergySolutionsBackendApplication {
-
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+        System.setProperty("SOLAR_API", dotenv.get("SOLAR_API"));
+        System.setProperty("BEARER_TOKEN", dotenv.get("BEARER_TOKEN"));
+
         SpringApplication.run(SmartEnergySolutionsBackendApplication.class, args);
     }
-
 }
--- src/main/java/org/sesolutions/smartenergysolutionsbackend/configuration/WebClientConfig.java
+++ src/main/java/org/sesolutions/smartenergysolutionsbackend/configuration/WebClientConfig.java
@@ -9,8 +9,8 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("https://api.solcast.com.au/data/historic/radiation_and_weather")
-                .defaultHeader("Authorization", "Bearer 7FkbI7yN0lXtgvviQVvfAUlUnWoy2fFp")
+                .baseUrl(System.getProperty("SOLAR_API"))
+                .defaultHeader("Authorization", System.getProperty("BEARER_TOKEN"))
                 .defaultHeader("Content-Type", "application/json")
                 .build();
     }
